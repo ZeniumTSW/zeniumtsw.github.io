0a00f483b2ba67da79d0e11b0090c23196a79287
zeniumtsw <zeniumtsw@gmail.com>
2019-11-14 16:57:04 +0300

    d

---
+++ js/scriptsrc.js
@@ -0,0 +1,13 @@
+setInterval(function() {
+  document.getElementById("ds3").width = document.getElementById("ds4").width;
+
+  document.getElementById("ds3").height = document.getElementById("ds4").height;
+
+  document.getElementById("ds2").width = document.getElementById("ds4").width;
+
+  document.getElementById("ds2").height = document.getElementById("ds4").height;
+
+  document.getElementById("ds1").width = document.getElementById("ds4").width;
+
+  document.getElementById("ds1").height = document.getElementById("ds4").height;
+}, 500);
